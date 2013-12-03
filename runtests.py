d0025d0a56d6368f41be8d4d8a04caa4cbb835e6
Piper Merriam <piper@simpleenergy.com>
2013-12-03 15:09:34 -0700

    Configure travis to run the tests on mysql and postgres

--- runtests.py
+++ runtests.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python
 import sys
+import os
 
 from django.conf import settings
 
-
-settings.configure(
+if os.environ.get('DB_BACKEND') == 'mysql':
+    DATABASES = {
+        'default': {
+            'NAME': 'test_db',
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'travis',
+        }
+    }
+elif os.environ.get('DB_BACKEND') == 'postgres':
     DATABASES={
         'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory;'}
     },
+    DATABASES = {
+        'default': {
+            'NAME': 'test_db',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'USER': 'postgres',
+        }
+    }
+else:
+    DATABASES={
+        'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory;'}
+    },
+
+settings.configure(
+    DATABASES=DATABASES,
     INSTALLED_APPS=[
         'django.contrib.contenttypes',
         'epochdatetimefield',
